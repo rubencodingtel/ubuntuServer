6fce2f6c7c5276dcbb288509f3bcdbf1d9178d2b
rubencodingtel <59182127+rubencodingtel@users.noreply.github.com>
2021-07-16 12:51:48 -0400

    firstcommit

---
+++ phpInstall.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# -*- ENCODING: UTF-8 -*-
+
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt update
+
+sudo apt install -y php7.4
+
+sudo apt install php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl
+
+php -v
+
+exit
