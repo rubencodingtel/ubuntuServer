3a48d29435e7aa4daa12791ee42f5c8f6f7a31c7
rubencodingtel <59182127+rubencodingtel@users.noreply.github.com>
2021-08-08 19:55:48 -0400

    Update phpInstall.sh

--- phpInstall.sh
+++ phpInstall.sh
@@ -10,7 +10,7 @@ sudo apt update
 
 sudo apt install -y php7.4
 
-sudo apt install php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl php-cli unzip
+sudo apt install php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl php7.4-cli unzip
 
 php -v
 
