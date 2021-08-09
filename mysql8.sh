b3bb791a4965090d92c97b3b5183487764485d8f
rubencodingtel <59182127+rubencodingtel@users.noreply.github.com>
2021-08-08 22:44:40 -0400

    Update mysql8.sh

--- mysql8.sh
+++ mysql8.sh
@@ -19,8 +19,6 @@ sudo dpkg -i mysql-community-server_8.0.26-1ubuntu20.04_amd64.deb
 
 sudo dpkg -i mysql-server_8.0.26-1ubuntu20.04_amd64.deb
 
-sudo apt install mysql-client mysql-community-server mysql-server
-
 sudo mysql_secure_installation
 
 exit
