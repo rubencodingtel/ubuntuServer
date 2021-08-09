a6e9e9ca2bc153b0542025e83ff74271fb140c33
rubencodingtel <59182127+rubencodingtel@users.noreply.github.com>
2021-08-08 23:07:33 -0400

    Update mysql8.sh

--- mysql8.sh
+++ mysql8.sh
@@ -7,13 +7,21 @@ sudo dpkg -i mysql-apt-config_0.8.17-1_all.deb
 
 sudo apt-get update
 
+sudo apt-get install libmecab2
+
 sudo apt-cache policy mysql-server
 
-sudo dpkg -i libmysqlclient-dev_8.0.26-1ubuntu20.04_amd64.deb
+sudo dpkg -i mysql-common_8.0.26-1ubuntu20.04_amd64.deb
+
+sudo dpkg -i mysql-community-client-plugins_8.0.26-1ubuntu20.04_amd64.deb
 
 sudo dpkg -i libmysqlclient21_8.0.26-1ubuntu20.04_amd64.deb
 
-sudo dpkg -i mysql-client_8.0.26-1ubuntu20.04_amd64.deb
+sudo dpkg -i mysql-community-client-core_8.0.26-1ubuntu20.04_amd64.deb
+
+sudo dpkg -i mysql-community-client_8.0.26-1ubuntu20.04_amd64.deb
+
+sudo dpkg -i mysql-community-server-core_8.0.26-1ubuntu20.04_amd64.deb
 
 sudo dpkg -i mysql-community-server_8.0.26-1ubuntu20.04_amd64.deb
 
